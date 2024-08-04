be627443696f0571aca1225796390a69232ce93e
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-04 19:25:24 +0530

    feat: Add candidate routes and CRUD operations

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 // Import the router files
 const userRoutes = require('./routes/userRoutes');
-// const candidateRoutes = require('./routes/candidateRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
 
 // Use the routers
 app.use('/user', userRoutes);
-// app.use('/candidate', candidateRoutes);
+app.use('/candidate', candidateRoutes);
 
 
 app.listen(PORT, ()=>{
