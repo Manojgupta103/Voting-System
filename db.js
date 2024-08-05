6e534f2017f670b20bf2e55a7f288b8a11ac72ed
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-05 23:35:59 +0530

    feat: Update JWT_SECRET in .env file

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL_LOCAL; // Replace 'mydatabase' with your database name
 // const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Get the default connection
 // Mongoose maintains a default connection object representing the MongoDB connection.
@@ -17,16 +17,16 @@ const db = mongoose.connection;
 
 // Define event listeners for database connection
 
-db.on('connected', () => {
-    console.log('Connected to MongoDB server');
+db.on("connected", () => {
+  console.log("Connected to MongoDB server");
 });
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
-db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
 });
 
 // Export the database connection
