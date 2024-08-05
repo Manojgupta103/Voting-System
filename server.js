ec0c8fb28a448df8566c510331b7361bd3abbf6b
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-05 23:36:22 +0530

    Refactor server.js for improved code readability and consistency

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const db = require('./db');
-require('dotenv').config();
+const db = require("./db");
+require("dotenv").config();
 
-const bodyParser = require('body-parser'); 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
 // Import the router files
-const userRoutes = require('./routes/userRoutes');
-const candidateRoutes = require('./routes/candidateRoutes');
+const userRoutes = require("./routes/userRoutes");
+const candidateRoutes = require("./routes/candidateRoutes");
 
 // Use the routers
-app.use('/user', userRoutes);
-app.use('/candidate', candidateRoutes);
+app.use("/user", userRoutes);
+app.use("/candidate", candidateRoutes);
 
-
-app.listen(PORT, ()=>{
-    console.log('listening on port 3000');
-})
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
